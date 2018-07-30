bbe3c771a33ef946a929c737030be4365d5dfa60
isheebo <kedgar751@gmail.com>
2018-07-30 16:07:23 +0300

    working on amity

--- app/amity.py
+++ app/amity.py
@@ -14,6 +14,8 @@ class Amity:
         self.employees = []
         self.available_offices = []
         self.available_livingspaces = []
+        self.all_rooms = []
+        self.all_room_names = []
 
     def add_person(self, name, person_type, wants_accommodation):
         """
@@ -21,6 +23,7 @@ class Amity:
         It can go ahead and grant them rooms depending on their person type.
         """
         person = None
+        # what do we do for existing fellows and staff, do we re-add them
         if person_type == 'Fellow':
             person = Fellow(name, wants_accommodation)
         elif person_type == 'Staff':
@@ -31,10 +34,10 @@ class Amity:
 
         if person is not None:
             self.employees.append(person)
-            self.allocate_person_room(person)
+            self._allocate_person_room(person)
         return True
 
-    def allocate_person_room(self, person):
+    def _allocate_person_room(self, person):
         """
         Assumes that we have some rooms available to the system.
         Allocates a newly created person a room basing on their person type.
@@ -45,3 +48,7 @@ class Amity:
         if person.type == 'Fellow' and person.wants_accommodation:
             livingspace = random.choice(self.available_livingspaces)
             livingspace.add_person(person)
+
+    def create_room(self, name, room_type):
+        if name.lower() in self.all_room_names:
+            return False
